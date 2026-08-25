85baf4082bdfba41f8e12fb9743e18cdbe82ea0e
hajir1 <paksutrisno934@gmail.com>
2026-08-25 16:53:33 +0700

    footer

--- wppusher-test.php
+++ wppusher-test.php
@@ -10,6 +10,6 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 // Load semua file shortcode dari tiap folder
 require_once plugin_dir_path( __FILE__ ) . 'footer/footer.php';
-require_once plugin_dir_path( __FILE__ ) . 'footer/menu.php';
+require_once plugin_dir_path( __FILE__ ) . 'menu/menu.php';
 // require_once plugin_dir_path( __FILE__ ) . 'menu/Menu.php';     (kalau nanti ada)
 // require_once plugin_dir_path( __FILE__ ) . 'navbar/Navbar.php'; (kalau nanti ada)
